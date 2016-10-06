0dc6674e222fc8f4206fd0064fca83ca01e7c6d0
INSGB-15 <INSGB-15@DESKTOP-MT1O7UK>
2016-10-06 12:04:06 +0100

    commit

--- XamarinListViewContextMenu/Resources/Resource.Designer.cs
+++ XamarinListViewContextMenu/Resources/Resource.Designer.cs
@@ -61,7 +61,13 @@ namespace XamarinListViewContextMenu
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int DetailLayout = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Model = 2130903042;
 			
 			static Layout()
 			{
